5aa6bfa16750689f9a8a1ab92594a0183e0b986c
Rafael Sanzio <rafael.rocha@wexinc.com>
2023-03-03 09:19:17 -0300

    feat: adding comment component
    Adding a new component for comments

---
+++ src/components/Comment.tsx
@@ -0,0 +1,63 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
+
+import { Avatar } from "./Avatar";
+
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/rafaelsanzio.png"
+        alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Rafael Sanzio</strong>
+              <time
+                title="30 de Janeiro às 10:30h"
+                dateTime="2023-01-30 10:30:30"
+              >
+                Cerca há 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
